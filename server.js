03233c2c6f47e1f30747016e579b168aa89724ae
gabrielhipolitoo <c.gabrielhipolitoh@gmail.com>
2024-06-26 21:16:43 -0300

    server, create emit between sockets serve and client

--- server.js
+++ server.js
@@ -3,33 +3,49 @@ import { Server } from 'socket.io'
 import http from 'http'
 import chalk from 'chalk'
 
+// game
+import createGame from './public/createGame.js'
+const game = createGame()
 class CreateServer {
   constructor() {
-    this.startServer()
-    this.socketsOn(this.sockets)
+    const dataConnection = this.startServer().sockets
+    this.socketsOn(dataConnection)
   }
 
   startServer() {
     const app = express()
     const httpServer = http.createServer(app)
     const sockets = new Server(httpServer)
-    try {
-      const port = 3000
-      httpServer.listen(port, () => {
-        console.log(chalk.yellow('Server running on port -->', port))
-      })
-    } catch (error) {
-      console.log(chalk.red('FALIED --> OPS O SERVIDOR FALHOU ! ', error))
-    }
+    const port = 3000
+
+    app.use(express.static('public'))
+
+    httpServer.listen(port, () => {
+      console.log(chalk.yellow('Server running on port -->', port))
+    })
 
     return {
+      httpServer,
       sockets,
     }
   }
 
   socketsOn(sockets) {
-    console.log('to aqui')
-    console.log(sockets)
+    game.obsorvers.subscribe((data) => {
+      console.log('Emmiting -->', data.type)
+      sockets.emit(data.type, data.command)
+    })
+    sockets.on('connection', (socket) => {
+      const playerObj = {
+        playerId: socket.id,
+        playerColor: 'red',
+        playerX: 5,
+        playerY: 4,
+      }
+      game.addPlayers(playerObj)
+    })
+
+    sockets.emit('setup-game', game.state)
   }
 }
 
